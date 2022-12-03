37caa4444599fc0c01d929b12c253988b59a6a1a
Vagner Kuntz <vaagner.kuntz@gmail.com>
2022-12-03 11:30:36 -0300

    test: object to query string

---
+++ project1/src/lib/queryString.spec.js
@@ -0,0 +1,18 @@
+const { queryString } = require('./queryString')
+
+describe('Object to query string', () => {
+    it('should create a valid query string when an object is provided', () => {
+        const obj = {
+            name: 'Vagner',
+            profession: 'Developer'
+        }
+
+        expect(queryString(obj)).toBe(
+            'name=Vagner&profession=Developer'
+        )
+    }); 
+});
+
+// describe('Query string to object', () => {
+    
+// });
