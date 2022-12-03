a7d7e90385f56b2e324d1eb3adc1641f416f9ca3
Vagner Kuntz <vaagner.kuntz@gmail.com>
2022-12-03 11:55:49 -0300

    chore: add new tests and refactor function

--- project1/src/lib/queryString.spec.js
+++ project1/src/lib/queryString.spec.js
@@ -9,6 +9,29 @@ describe('Object to query string', () => {
 
     expect(queryString(obj)).toBe('name=Vagner&profession=Developer');
   });
+
+  it('should create a valid query string even when an array is passed as value', () => {
+    const obj = {
+      name: 'Vagner',
+      abilities: ['JS', 'TDD'],
+    };
+
+    expect(queryString(obj)).toBe('name=Vagner&abilities=JS,TDD');
+  });
+
+  it('should throw an error when an object is passed as value', () => {
+    const obj = {
+      name: 'Vagner',
+      abilities: {
+        first: 'JS',
+        second: 'TDD',
+      },
+    };
+
+    expect(() => {
+      queryString(obj);
+    }).toThrowError();
+  });
 });
 
 // describe('Query string to object', () => {
