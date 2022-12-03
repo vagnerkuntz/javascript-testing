114a7874fc8dff7e47e50d29201353e46a3e2768
Vagner Kuntz <vaagner.kuntz@gmail.com>
2022-12-03 11:35:30 -0300

    chore: add prettier

--- project1/src/lib/queryString.spec.js
+++ project1/src/lib/queryString.spec.js
@@ -1,18 +1,16 @@
-const { queryString } = require('./queryString')
+const { queryString } = require('./queryString');
 
 describe('Object to query string', () => {
-    it('should create a valid query string when an object is provided', () => {
-        const obj = {
-            name: 'Vagner',
-            profession: 'Developer'
-        }
+  it('should create a valid query string when an object is provided', () => {
+    const obj = {
+      name: 'Vagner',
+      profession: 'Developer',
+    };
 
-        expect(queryString(obj)).toBe(
-            'name=Vagner&profession=Developer'
-        )
-    }); 
+    expect(queryString(obj)).toBe('name=Vagner&profession=Developer');
+  });
 });
 
 // describe('Query string to object', () => {
-    
+
 // });
