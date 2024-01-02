6ac0c8ecb9660195133d7d23ddcddb51c8b90269
Vagner Kuntz <vaagner.kuntz@gmail.com>
2024-01-02 14:12:58 -0300

    chore: ensure that a product only appears once in the cart

--- module01-simple-lib/src/lib/cart.spec.js
+++ module01-simple-lib/src/lib/cart.spec.js
@@ -2,6 +2,10 @@ import Cart from "./Cart";
 
 describe('Cart', () => {
   let cart;
+  let product = {
+    title: 'Adidas',
+    price: 35388 // 353.88 | R$ 353,88
+  }
 
   beforeEach(() => {
     cart = new Cart();
@@ -13,14 +17,25 @@ describe('Cart', () => {
 
   it('should multiply quantity and price and receive the total amount', () => {
     const item = {
-      product: {
-        title: 'Adidas',
-        price: 35388 // 353.88 | R$ 353,88
-      },
+      product,
       quantity: 2 // 707.76 | R$ 707,76
     }
 
     cart.add(item);
     expect(cart.getTotal()).toEqual(70776);
+  })
+
+  it('should ensure no more than on product exists at a time', () => {
+    cart.add({
+      product,
+      quantity: 2
+    });
+
+    cart.add({
+      product,
+      quantity: 1
+    });
+
+    expect(cart.getTotal()).toEqual(35388);
   })
 });
