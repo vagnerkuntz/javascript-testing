7648392bdbb9e5b5f699efe7f683b77ced3ea286
Vagner Kuntz <vaagner.kuntz@gmail.com>
2024-01-11 21:10:19 -0300

    chore: second discount condition - second use case

--- module01-simple-lib/src/lib/cart.spec.js
+++ module01-simple-lib/src/lib/cart.spec.js
@@ -102,7 +102,7 @@ describe('Cart', () => {
 
       expect(cart.getTotal().getAmount()).toEqual(0);
     });
-  })
+  });
 
   describe('special conditions', () => {
     it('should apply percentage discount quantity above minimum is passed', () => {
@@ -118,7 +118,22 @@ describe('Cart', () => {
       });
 
       expect(cart.getTotal().getAmount()).toEqual(74315);
-    })
+    });
+
+    it('should NOT apply percentage discount quantity is below or equals minimum', () => {
+      const condition = {
+        percentage: 30,
+        minimum: 2
+      }
+
+      cart.add({
+        product,
+        quantity: 2,
+        condition
+      });
+
+      expect(cart.getTotal().getAmount()).toEqual(70776);
+    });
 
     it('should apply quantity discount for even quantities', () => {
       const condition = {
@@ -132,6 +147,34 @@ describe('Cart', () => {
       });
 
       expect(cart.getTotal().getAmount()).toEqual(70776);
-    })
+    });
+
+    it('should NOT apply quantity discount for even quantities when condition is not met', () => {
+      const condition = {
+        quantity: 2,
+      }
+
+      cart.add({
+        product,
+        quantity: 1,
+        condition
+      });
+
+      expect(cart.getTotal().getAmount()).toEqual(35388);
+    });
+
+    it('should apply quantity discount for odd quantities', () => {
+      const condition = {
+        quantity: 2,
+      }
+
+      cart.add({
+        product,
+        quantity: 5,
+        condition
+      });
+
+      expect(cart.getTotal().getAmount()).toEqual(106164);
+    });
   });
 });
